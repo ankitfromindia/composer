f89bfbbd0cde33cfc412eeccac6adbaa5fac48ce
Ankit Vishwakarma <er.ankitvishwakarma@gmail.com>
2017-03-07 01:31:02 +0530

    First Commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HelloWorld;
+/**
+ * Description of SayHello
+ *
+ * @author ankitvishwakarma
+ */
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
